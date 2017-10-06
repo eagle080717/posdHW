70f55f29bc3c67ce68b7c7a4a897f0fb81f9eadd
eagle080717 <32092114+eagle080717@users.noreply.github.com>
2017-10-06 18:20:52 +0800

    20171006

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <gtest/gtest.h>
 #include "utTerm.h"
 
@@ -7,13 +6,3 @@ int main( int argc , char **argv )
     testing :: InitGoogleTest( &argc , argv ) ;
     return RUN_ALL_TESTS( ) ;
 }
-=======
-#include <gtest/gtest.h>
-#include "utSort.h"
-
-int main( int argc , char **argv )
-{
-    testing :: InitGoogleTest( &argc , argv ) ;
-    return RUN_ALL_TESTS( ) ;
-}
->>>>>>> 6609119d1eb49b377bcf20c9302cfb855cb8253a
