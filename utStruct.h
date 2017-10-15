d0c7c1290e96d3996f254b55f65071d7ce9eff42
eagle080717 <32092114+eagle080717@users.noreply.github.com>
2017-10-15 23:34:51 +0800

    Add files via upload

--- utStruct.h
+++ utStruct.h
@@ -1,6 +1,8 @@
 #include <vector>
 #include "atom.h"
 #include "struct.h"
+#include "number.h"
+#include "variable.h"
 
 TEST(Struct, hobby)
 {
@@ -14,7 +16,7 @@ TEST(Struct, hobby)
 
 }
 
-TEST(Struct,symbol)
+TEST(Struct, symbol)
 {
   Atom tom("tom");
   Atom chaseMouse("chaseMouse");
@@ -23,7 +25,7 @@ TEST(Struct,symbol)
   ASSERT_EQ("hobby(tom, chaseMouse)",hobby.symbol());
 }
 
-TEST(Struct,match1)
+TEST(Struct, match1)
 {
   Atom tom("tom");
   Atom chaseMouse("chaseMouse");
@@ -32,7 +34,8 @@ TEST(Struct,match1)
   Struct hobby2(Atom("hobby2"), v);
   EXPECT_FALSE(hobby.match(hobby2));
 }
-TEST(Struct,match2){
+
+TEST(Struct, match2){
   Atom tom("tom");
   Atom chaseMouse("chaseMouse");
   std::vector<Term *> v = {&tom, &chaseMouse};
@@ -41,7 +44,8 @@ TEST(Struct,match2){
   Struct hobby2(Atom("hobby"), v1);
   EXPECT_FALSE(hobby.match(hobby2));
 }
-TEST(Struct,match3){
+
+TEST(Struct, match3){
   Atom tom("tom");
   Atom chaseMouse("chaseMouse");
   std::vector<Term *> v = {&tom, &chaseMouse};
@@ -50,7 +54,8 @@ TEST(Struct,match3){
   Struct hobby2(Atom("hobby"), v1);
   EXPECT_FALSE(hobby.match(hobby2));
 }
-TEST(Struct,match4)
+
+TEST(Struct, match4)
 {
   Atom tom("tom");
   Atom chaseMouse("chaseMouse");
@@ -59,7 +64,8 @@ TEST(Struct,match4)
   Struct hobby2(Atom("hobby"), v);
   EXPECT_TRUE(hobby.match(hobby2));
 }
-TEST(Struct,match5)
+
+TEST(Struct, match5)
 {
   Atom tom("tom");
   Atom chaseMouse("chaseMouse");
@@ -67,3 +73,62 @@ TEST(Struct,match5)
   Struct hobby(Atom("hobby"), v);
   EXPECT_FALSE(hobby.match(tom));
 }
+
+// When Struct s contains a Variable X
+// Then #symbol() should return "s(X)"
+// and #value() should also return "s(X)"
+TEST(Struct, var)
+{
+
+}
+
+// Given there is Struct s contains a Variable X
+// When Variable X matches an Atom "tom"
+// Then #symbol() should return "s(X)"
+// and #value() should also return "s(tom)"
+TEST(Struct, var_match_atom)
+{
+
+}
+
+// Given there are Struct s1 and Struct s2
+// When Struct s1 contains Struct s2
+// And Struct s2 contains a Variable X
+// Then #symbol() of s1 should return "s1(s2(X))"
+// and #value() of s1 should also return "s1(s2(X))"
+TEST(Struct, nested_struct1)
+{
+
+}
+
+// Given there are Struct s1 contains Struct s2
+// And Struct s2 contains Variable X
+// When Variable X matches an Atom "tom"
+// Then #symbol() of s1 should return "s1(s2(X))"
+// and #value() of s1 should return "s1(s2(tom))"
+TEST(Struct, nested_struct2)
+{
+
+}
+
+// Given there are Struct s1 contains Struct s2
+// And Struct s2 contains Variable X
+// When Variable X matches an Number pi: 3.14
+// Then #symbol() of s1 should return "s1(s2(X))"
+// and #value() of s1 should return "s1(s2(3.14))"
+TEST(Struct, nested_struct3)
+{
+
+}
+
+// Given there are Struct s1 contains Struct s2 and Variable X
+// And Struct s2 contains Variable Y
+// When Variable X matches Variable Y
+// And Variable X matches Atom "kent_beck"
+// Then #symbol() of s1 should return "s1(s2(Y), X)"
+// and #value() of s1 should return "s1(s2(kent_beck), kent_beck)"
+TEST(Struct, nested_struct_and_multiVariable)
+{
+
+}
+
