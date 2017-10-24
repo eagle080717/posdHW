3afc448382f945c8a7ed9563b3c2a64eef24c239
eagle080717 <32092114+eagle080717@users.noreply.github.com>
2017-10-24 14:00:13 +0800

    Upload this sh*t via my Nokia 3310

--- list.h
+++ list.h
@@ -2,21 +2,66 @@
 #define LIST_H
 
 #include "term.h"
-
+#include <string>
 #include <vector>
 using std::vector;
+using std::string;
 
 class List : public Term {
 public:
-  string symbol() const;
-  string value() const;
-  bool match(Term & term);
+  string symbol() const {
+  	// To do: sorting the numbers
+  	if(!_elements.empty()){
+  		string str = "[";
+  		for(auto iter=_elements.begin(); iter != iter.end(); ++iter){
+  			str += *iter.symbol();
+  			str += ", ";
+  		}
+  		//str = str.substr(0, str.size()-2);
+  		str.pop_back();
+  		str.pop_back();
+  		str += "]";
+  		return str;
+  	}
+  	else	
+  		return "[]";
+  }
+
+  string value() const {
+  	// To do: sorting the numbers
+  	if(!_elements.empty()){
+  		string str = "[";
+  		for(auto iter=_elements.begin(); iter != iter.end(); iter++){
+  			str += *iter.value();
+  			str += ", ";
+  		}
+  		str.pop_back();
+  		str.pop_back();
+  		str += "]";
+  		return str;
+  	}
+  	else	
+  		return "[]";
+  }
+
+  bool match(Term &term) {
+  	//Check whether is a list or not, if not, then return false, otherwise, comparing the list element
+  	
+  }
 
 public:
   List (): _elements() {}
-  List (vector<Term *> const & elements):_elements(elements){}
-  Term * head() const;
-  List * tail() const;
+
+  List (vector<Term *> const &elements):_elements(elements){}
+
+  Term * head() const {
+  	auto iter = _elements.begin();
+  	return *iter;
+  }
+
+  List * tail() const {
+  	return _elements.back();
+  }
 
 private:
   vector<Term *> _elements;
